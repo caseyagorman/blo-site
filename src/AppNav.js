61170822a8e89be1622ba78988601d503ca58887
Casey Gorman <caseyagorman@gmail.com>
2019-04-07 18:49:20 -0700

    add bootstrap to form

--- src/AppNav.js
+++ src/AppNav.js
@@ -1,8 +1,8 @@
 import React from "react"
 import {Navbar, Nav,} from "react-bootstrap"
-
+import "./app.css"
 const AppNav = () => (
-<Navbar style={{backgroundColor: "black", color: "white"}}>
+<Navbar style={{backgroundColor: "black", color: "white", fontFamily: "Open Sans"}}>
     <Navbar.Brand  href="/" style={{ color: "white"}}>BLO</Navbar.Brand>
     <Nav className="mr-auto" style={{color: "white"}}>
       <Nav.Link href="/about" style={{ color: "white", margin: 20}}>About</Nav.Link>
--- src/Form.js
+++ src/Form.js
@@ -1,63 +1,85 @@
 import React, {Component} from "react"
-import {Form} from "react-bootstrap"
+import {Form, FormControl, Button} from "react-bootstrap"
 import {Link} from "react-router-dom"
 
 class SignupForm extends Component {
     constructor(props){
         super(props)
-        this.state = {name: null, email: null, instrument: null, about: null}
+        this.state = {name: "", email: "", instrument: "", about: "", agree: true}
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    handleChange(e){
-        e.preventDefault()
-        this.setState({[e.target.name]: e.target.value})
-    }
+
+
+    handleChange(e) {
+        const target = e.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
+
     handleSubmit(e){
         e.preventDefault()
-
+        console.log("submmited", this.state)
     }
 
     render(){
         return (
+            <div className="container">
+            <h1 style={{textAlign: "center"}}>Membership Request Form</h1>
+            <div style={{textAlign: "center"}}>If you see us on the street, we are always open to folks coming up and playing with us. 
+            If you want to get more involved, please fill out the form below. 
+            B.L.O. is open to all musical levels. 
+            Please check out our politics and look over some of our past actions to see 
+            if you think we would be a good fit.</div>
+            <br></br>
             <Form onSubmit={this.handleSubmit}>
-            <input 
+            <FormControl 
             onChange={this.handleChange}
             name="name"
             type="text"
             placeholder="name"
             value={this.state.name}
             />
-            <input 
+            <br></br>
+            <FormControl 
             onChange={this.handleChange}
             name="email"
             type="email"
             placeholder= "email"
             value={this.state.email}
             />
-            <input 
+            <br></br>
+            <FormControl 
             onChange={this.handleChange}
             name="instrument"
             type="text"
             placeholder="instrument"
             value={this.state.instrument}
             />
-               <input 
+            <br></br>
+               <FormControl 
             onChange={this.handleChange}
             name="about"
             type="textarea"
             placeholder="tell us about you"
             value={this.state.about}
             />
-            <label> Agree to <Link to={"/about"}>  points of unity?</Link> </label>
-            <input onChange={this.handleChange}
+            <br></br>
+            <label style={{fontSize: 16, marginRight: 5}}> Agree to <Link to={"/about"} style={{color: "#ef0067"}}>  points of unity?</Link> </label>
+
+        <input onChange={this.handleChange}
             name="agree"
             type="checkbox"
             value={this.state.agree}/>
-
-            <button>submit</button>
+            <br></br>
+            <Button style={{backgroundColor:"#ef0067", border: "1px solid black"}}>submit</Button>
             </Form>
+            </div>
         )
     }
 }
